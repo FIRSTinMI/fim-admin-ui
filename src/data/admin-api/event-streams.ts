c22d51eabab1683a543113e2f9be9e3d3167d951
Soren Zaiser <soren.zaiser@gmail.com>
2025-11-20 01:06:47 -0500

    Youtube integration and more!

--- src/data/admin-api/event-streams.ts
+++ src/data/admin-api/event-streams.ts
@@ -34,3 +34,36 @@ export const useRunEventStreamSetter = () => useSupaMutation({
       { eventIds }: { eventIds: string[] }
     ) => runEventStreamSetter(client, eventIds)
 });
+
+
+const livestreamDelete = async (
+  client: FimSupabaseClient,
+  livestreamId: string
+) => {
+  return fetch(
+    `${import.meta.env.PUBLIC_ADMIN_API_URL}/api/v1/event-streams/${livestreamId}`,
+    {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${
+          (await client.auth.getSession()).data.session?.access_token
+        }`,
+      },
+    }
+  ).then(async (resp) => {
+    if (resp.status === 401 || resp.status === 403)
+      throw new Error("You do not have permission to perform this action.");
+    if (!resp.ok)
+      throw new Error(
+        `An error occurred while setting the event streams: ${resp.statusText}`
+      );
+  });
+};
+
+export const useLivestreamDelete = () => useSupaMutation({
+    mutationFn: (
+      client: FimSupabaseClient,
+      { livestreamId }: { livestreamId: string }
+    ) => livestreamDelete(client, livestreamId)
+});
